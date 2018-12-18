02e266b9cb3366f9c44195122263601c0e4f9ed8
student <student@imac-75.local>
2018-12-18 14:00:42 +0800

    final

--- thirteenth(2)/AppDelegate.swift
+++ thirteenth(2)/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  WebView
+//  thirteenth(2)
 //
 //  Created by student on 2018/12/17.
-//  Copyright © 2018年 yx. All rights reserved.
+//  Copyright © 2018年 qiuyue. All rights reserved.
 //
 
 import UIKit
